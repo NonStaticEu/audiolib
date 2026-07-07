2f2eba8fb8bd0c0a88e0dbe92856047775a01c21
fabmars <fabien@nonstatic.eu>
2026-07-07 04:49:54 +0200

    Added missing headers

--- src/main/java/eu/nonstatic/audio/formats/dts/DtsInfo.java
+++ src/main/java/eu/nonstatic/audio/formats/dts/DtsInfo.java
@@ -1,3 +1,12 @@
+/**
+ * Audiolib
+ * Copyright (C) 2022 NonStatic
+ *
+ * This file is part of audiolib.
+ * Audiolib is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *  is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with . If not, see <https://www.gnu.org/licenses/>.
+ */
 package eu.nonstatic.audio.formats.dts;
 
 import eu.nonstatic.audio.AudioFileType;
@@ -18,7 +27,7 @@ public class DtsInfo implements AudioInfo {
   private final Duration duration;
 
   public DtsInfo(WaveInfo infos) {
-    this(infos.getName(), infos.getWaveFormat(), infos.getNumChannels(), infos.getSampleRate(), infos.getBitsPerSample(), infos.getAudioSize(), infos.getDuration());
+    this(infos.getName(), infos.getFormat(), infos.getNumChannels(), infos.getSampleRate(), infos.getBitsPerSample(), infos.getAudioSize(), infos.getDuration());
   }
 
   @Override
